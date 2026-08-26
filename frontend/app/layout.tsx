6e1112ab28f74e4c339018fb4703773373c52db6
Adil Ishtiaq <adilishtiaq8040@gmail.com>
2026-08-27 00:53:32 +0500

    Suppress false-positive hydration warning from browser extensions
    cz-shortcut-listen (ColorZilla) and similar extensions inject attributes
    into <body> before React hydrates, which Next.js reports as a hydration
    mismatch even though nothing in the app is actually wrong.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${plusJakartaSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <TooltipProvider>
           <SidebarProvider>
             <AppSidebar />
